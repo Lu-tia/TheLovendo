836548353fef9391b9d0a59d08c61c16e26aea0e
Pasquale Abate <lino.abate@gmail.com>
2024-07-02 20:45:47 +0200

    creata rotta articles index

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,11 @@
 
 use App\Http\Controllers\ArticleController;
 use App\Http\Controllers\PageController;
+use App\Models\Article;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/' ,[PageController::class, 'homepage'])->name('homepage');
 
-Route::get('/articolo/crea',[ArticleController::class, 'create'])->name('articles.create');
-
+Route::get('/articoli/crea',[ArticleController::class, 'create'])->name('articles.create');
+Route::get('/articoli',[ArticleController::class, 'index'])->name('articles.index');
 
