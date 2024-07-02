f59b9560bbb9e0f86cf81d3c5e3b17ab9c521a5b
Giammarco Talò <giammarcotalo@gmail.com~>
2024-07-02 21:10:24 +0200

    tradotto e collegato link, ripulito

--- resources/views/components/layouts/navbar.blade.php
+++ resources/views/components/layouts/navbar.blade.php
@@ -8,125 +8,70 @@
                             <img src="assets/images/logo/logo.svg" alt="Logo">
                         </a>
                         <button class="navbar-toggler mobile-menu-btn" type="button" data-bs-toggle="collapse"
-                            data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-                            aria-expanded="false" aria-label="Toggle navigation">
-                            <span class="toggler-icon"></span>
-                            <span class="toggler-icon"></span>
-                            <span class="toggler-icon"></span>
-                        </button>
-                        <div class="collapse navbar-collapse sub-menu-bar" id="navbarSupportedContent">
-                            <ul id="nav" class="navbar-nav ms-auto">
-                                <li class="nav-item">
-                                    <a class="active dd-menu collapsed" href="javascript:void(0)"
-                                        data-bs-toggle="collapse" data-bs-target="#submenu-1-1"
-                                        aria-controls="navbarSupportedContent" aria-expanded="false"
-                                        aria-label="Toggle navigation">Home</a>
-                                    <ul class="sub-menu collapse" id="submenu-1-1">
-                                        <li class="nav-item active"><a href="index.html">Home Default</a></li>
-                                        <li class="nav-item"><a href="index2.html">Home Version 2</a></li>
-                                        <li class="nav-item"><a href="index3.html">Home Version 3</a></li>
-                                    </ul>
-                                </li>
-                                <li class="nav-item">
-                                    <a href="category.html" aria-label="Toggle navigation">Categories</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="dd-menu collapsed" href="javascript:void(0)" data-bs-toggle="collapse"
-                                        data-bs-target="#submenu-1-3" aria-controls="navbarSupportedContent"
-                                        aria-expanded="false" aria-label="Toggle navigation">Listings</a>
-                                    <ul class="sub-menu collapse" id="submenu-1-3">
-                                        <li class="nav-item"><a href="item-listing-grid.html">Ad Grid</a></li>
-                                        <li class="nav-item"><a href="item-listing-list.html">Ad Listing</a></li>
-                                        <li class="nav-item"><a href="item-details.html">Ad Details</a></li>
-                                    </ul>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="dd-menu collapsed" href="javascript:void(0)" data-bs-toggle="collapse"
-                                        data-bs-target="#submenu-1-4" aria-controls="navbarSupportedContent"
-                                        aria-expanded="false" aria-label="Toggle navigation">Pages</a>
-                                    <ul class="sub-menu mega-menu collapse" id="submenu-1-4">
-                                        <li class="single-block">
-                                            <ul>
-                                                <li class="mega-menu-title">Essential Pages</li>
-                                                <li class="nav-item"><a href="about-us.html">About Us</a></li>
-                                                <li class="nav-item"><a href="item-details.html">Ads Details</a></li>
-                                                <li class="nav-item"><a href="post-item.html">Ads Post</a></li>
-                                                <li class="nav-item"><a href="pricing.html">Pricing Table</a></li>
-                                                <li class="nav-item"><a href="registration.html">Sign Up</a></li>
-                                                <li class="nav-item"><a href="login.html">Sign In</a></li>
-                                                <li class="nav-item"><a href="contact.html">Contact Us</a></li>
-                                                <li class="nav-item"><a href="faq.html">FAQ</a></li>
-                                                <li class="nav-item"><a href="404.html">Error Page</a></li>
-                                                <li class="nav-item"><a href="mail-success.html">Mail Success</a>
-                                                </li>
-                                                <li class="nav-item"><a href="coming-soon.html">Comming Soon</a>
-                                                </li>
-                                            </ul>
-                                        </li>
-                                        <li class="single-block">
-                                            <ul>
-                                                <li class="mega-menu-title">Dashboard</li>
-                                                <li class="nav-item"><a href="dashboard.html">Account Overview</a>
-                                                </li>
-                                                <li class="nav-item"><a href="profile-settings.html">My Profile</a>
-                                                </li>
-                                                <li class="nav-item"><a href="my-items.html">My Ads</a></li>
-                                                <li class="nav-item"><a href="favourite-items.html">Favorite Ads</a>
-                                                </li>
-                                                <li class="nav-item"><a href="post-item.html">Ad post</a></li>
-                                                <li class="nav-item"><a href="bookmarked-items.html">Bookmarked Ad</a>
-                                                </li>
-                                                <li class="nav-item"><a href="messages.html">Messages</a></li>
-                                                <li class="nav-item"><a href="delete-account.html">Close account</a>
-                                                </li>
-                                                <li class="nav-item"><a href="invoice.html">Invoice</a></li>
-                                            </ul>
-                                        </li>
-                                    </ul>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="dd-menu collapsed" href="javascript:void(0)" data-bs-toggle="collapse"
-                                        data-bs-target="#submenu-1-5" aria-controls="navbarSupportedContent"
-                                        aria-expanded="false" aria-label="Toggle navigation">Blog</a>
-                                    <ul class="sub-menu collapse" id="submenu-1-5">
-                                        <li class="nav-item"><a href="blog-grid-sidebar.html">Blog Grid Sidebar</a>
-                                        </li>
-                                        <li class="nav-item"><a href="blog-single.html">Blog Single</a></li>
-                                        <li class="nav-item"><a href="blog-single-sidebar.html">Blog Single
-                                                Sibebar</a></li>
-                                    </ul>
-                                </li>
-                            </ul>
-                        </div> <!-- navbar collapse -->
-                        @guest
-
-                        <div class="login-button">
-                            <ul>
-                                <li>
-                                    <a href="{{route('login')}}"><i class="lni lni-enter"></i> Login</a>
-                                </li>
-                                <li>
-                                    <a href="{{route('register')}}"><i class="lni lni-user"></i> Register</a>
-                                </li>
-                            </ul>
-                        </div>
-                        @endguest
-
-                        @auth
-                        <span>Benvenuto, {{ auth()->user()->name }}</span>
-                        <a class="btn btn-sm btn-outline-secondary mx-2" href="{{ route('homepage') }}">Dashboard</a>
-
-                        <form method="POST" action="{{ route('logout') }}">
-                            @csrf
-                            <button class="btn btn-sm btn-outline-secondary mx-2" type="submit">Logout</button>
-                        </form>
-                        @endauth
-                        <div class="button header-button">
-                            <a href="post-item.html" class="btn">Post an Ad</a>
-                        </div>
-                    </nav> <!-- navbar -->
-                </div>
+                        data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                        aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="toggler-icon"></span>
+                        <span class="toggler-icon"></span>
+                        <span class="toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse sub-menu-bar" id="navbarSupportedContent">
+                        <ul id="nav" class="navbar-nav ms-auto">
+                            <li class="nav-item">
+                                <a class="active dd-menu collapsed" href="{{ route('homepage') }}"
+                                data-bs-toggle="collapse" data-bs-target="#submenu-1-1"
+                                aria-controls="navbarSupportedContent" aria-expanded="false"
+                                aria-label="Toggle navigation">Home</a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="{{ route('articles.index') }}" aria-label="Toggle navigation">Articoli</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="dd-menu collapsed" href="javascript:void(0)" data-bs-toggle="collapse"
+                                data-bs-target="#submenu-1-5" aria-controls="navbarSupportedContent"
+                                aria-expanded="false" aria-label="Toggle navigation">Categorie</a>
+                                <ul class="sub-menu collapse" id="submenu-1-5">
+                                    <li class="nav-item"><a href="#">Elettronica</a></li>
+                                    <li class="nav-item"><a href="#">Abbigliamento</a></li>
+                                    <li class="nav-item"><a href="#">Salute e Bellezza</a></li>
+                                    <li class="nav-item"><a href="#">Giocattoli</a></li>
+                                    <li class="nav-item"><a href="#">Sport</a></li>
+                                    <li class="nav-item"><a href="#">Animali Domestici</a></li>
+                                    <li class="nav-item"><a href="#">Libri e Riviste</a></li>
+                                    <li class="nav-item"><a href="#">Accessori</a></li>
+                                    <li class="nav-item"><a href="#">Motori</a></li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </div>
+                    @guest
+                    
+                    <div class="login-button">
+                        <ul>
+                            <li>
+                                <a href="{{route('login')}}"><i class="lni lni-enter"></i> Accedi</a>
+                            </li>
+                            <li>
+                                <a href="{{route('register')}}"><i class="lni lni-user"></i> Registrazione</a>
+                            </li>
+                        </ul>
+                    </div>
+                    @endguest
+                    
+                    @auth
+                    <span>Benvenuto, {{ auth()->user()->name }}</span>
+                    <a class="btn btn-sm btn-outline-secondary mx-2" href="{{ route('homepage') }}">Dashboard</a>
+                    
+                    <form method="POST" action="{{ route('logout') }}">
+                        @csrf
+                        <button class="btn btn-sm btn-outline-secondary mx-2" type="submit">Esci</button>
+                    </form>
+                    @endauth
+                    <div class="button header-button">
+                        <a href="post-item.html" class="btn">Crea un annuncio</a>
+                    </div>
+                </nav> <!-- navbar -->
             </div>
-        </div> <!-- row -->
-    </div> <!-- container -->
+        </div>
+    </div> <!-- row -->
+</div> <!-- container -->
 </header>
