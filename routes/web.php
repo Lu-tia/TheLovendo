14262b968540b049f30d6ff150778e85371c1387
Gabriele Zucchi <19gabrielezucchi99@gmail.com>
2024-07-02 21:27:13 +0200

    implementato middleware in web.php

--- routes/web.php
+++ routes/web.php
@@ -5,8 +5,12 @@ use App\Http\Controllers\PageController;
 use App\Models\Article;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/' ,[PageController::class, 'homepage'])->name('homepage');
+Route::get('/', [PageController::class, 'homepage'])->name('homepage');
 
-Route::get('/articoli/crea',[ArticleController::class, 'create'])->name('articles.create');
-Route::get('/articoli',[ArticleController::class, 'index'])->name('articles.index');
 
+Route::get('/articoli', [ArticleController::class, 'index'])->name('articles.index');
+
+Route::middleware(['auth'])->group(function () {
+
+    Route::get('/articoli/crea', [ArticleController::class, 'create'])->name('articles.create');
+});
