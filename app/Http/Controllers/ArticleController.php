3999486250f71b9db8f4e075f71b9a2822d6c9a1
Lucrezia Caricchia <lucrezia.caricchia@gmail.com>
2024-07-03 21:20:40 +0200

    updated

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -9,12 +9,17 @@ use Illuminate\Http\Request;
 class ArticleController extends Controller
 {
     public function create()
-    {   
+    {
         return view('articles.create');
     }
     public function index()
     {
         $articles = Article::orderBy('created_at', 'desc')->paginate(6);
         return view('articles.index', compact('articles'));
+    }
+
+    public function show()
+    {
+        return view('articles.show');
     }
 }
