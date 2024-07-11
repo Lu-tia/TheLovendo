8d846c8fc7c7ef7ed7c580d6b50bee22ebb3fa1d
Lucrezia Caricchia <lucrezia.caricchia@gmail.com>
2024-07-12 00:43:01 +0200

    aggiornamento frontend

--- resources/views/wishlist/index.blade.php
+++ resources/views/wishlist/index.blade.php
@@ -1,4 +1,182 @@
 <x-layouts.main>
+
+
+
+    <section class="category-page section mt-4">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-3 col-md-12 col-12">
+                    <!-- Start Dashboard Sidebar -->
+                    <x-user.sidebar />
+
+                </div>
+                <div class="col-lg-9 col-md-12 col-12">
+                    <div class="main-content">
+                        <div class="dashboard-block mt-0">
+                            <h3 class="block-title">Bookmarked Ads</h3>
+                            <!-- Start Items Area -->
+                            <div class="my-items">
+                                <!-- Start List Title -->
+                                <div class="item-list-title">
+                                    <div class="row align-items-center">
+                                        <div class="col-lg-5 col-md-5 col-12">
+                                            <p>Job Title</p>
+                                        </div>
+                                        <div class="col-lg-3 col-md-3 col-12">
+                                            <p>Category</p>
+                                        </div>
+                                        <div class="col-lg-2 col-md-2 col-12">
+                                            <p>Condition</p>
+                                        </div>
+                                        <div class="col-lg-2 col-md-2 col-12 align-right">
+                                            <p>Action</p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <!-- End List Title -->
+                                <!-- Start Single List -->
+                                <div class="single-item-list">
+                                    <div class="row align-items-center">
+                                        <div class="col-lg-5 col-md-5 col-12">
+                                            <div class="item-image">
+                                                <img src="https://via.placeholder.com/100x100" alt="#">
+                                                <div class="content">
+                                                    <h3 class="title"><a href="javascript:void(0)">Brand New Iphone 11
+                                                            Pro
+                                                            Max</a></h3>
+                                                    <span class="price">$800</span>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="col-lg-3 col-md-3 col-12">
+                                            <p>Electronic</p>
+                                        </div>
+                                        <div class="col-lg-2 col-md-2 col-12">
+                                            <p>New</p>
+                                        </div>
+                                        <div class="col-lg-2 col-md-2 col-12 align-right">
+                                            <ul class="action-btn">
+                                                <li><a href="javascript:void(0)"><i class="lni lni-eye"></i></a></li>
+                                                <li><a href="javascript:void(0)"><i class="lni lni-trash"></i></a></li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                </div>
+                                <!-- End Single List -->
+                                <!-- Start Single List -->
+                                <div class="single-item-list">
+                                    <div class="row align-items-center">
+                                        <div class="col-lg-5 col-md-5 col-12">
+                                            <div class="item-image">
+                                                <img src="https://via.placeholder.com/100x100" alt="#">
+                                                <div class="content">
+                                                    <h3 class="title"><a href="javascript:void(0)">New Ferrari F80
+                                                            Car</a>
+                                                    </h3>
+                                                    <span class="price">$13000</span>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="col-lg-3 col-md-3 col-12">
+                                            <p>Car</p>
+                                        </div>
+                                        <div class="col-lg-2 col-md-2 col-12">
+                                            <p>Vehicle</p>
+                                        </div>
+                                        <div class="col-lg-2 col-md-2 col-12 align-right">
+                                            <ul class="action-btn">
+                                                <li><a href="javascript:void(0)"><i class="lni lni-eye"></i></a></li>
+                                                <li><a href="javascript:void(0)"><i class="lni lni-trash"></i></a></li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                </div>
+                                <!-- End Single List -->
+                                <!-- Start Single List -->
+                                <div class="single-item-list">
+                                    <div class="row align-items-center">
+                                        <div class="col-lg-5 col-md-5 col-12">
+                                            <div class="item-image">
+                                                <img src="https://via.placeholder.com/100x100" alt="#">
+                                                <div class="content">
+                                                    <h3 class="title"><a href="javascript:void(0)">Brand New Iphone 11
+                                                            Pro
+                                                            Max</a></h3>
+                                                    <span class="price">$800</span>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="col-lg-3 col-md-3 col-12">
+                                            <p>Electronic</p>
+                                        </div>
+                                        <div class="col-lg-2 col-md-2 col-12">
+                                            <p>New</p>
+                                        </div>
+                                        <div class="col-lg-2 col-md-2 col-12 align-right">
+                                            <ul class="action-btn">
+                                                <li><a href="javascript:void(0)"><i class="lni lni-eye"></i></a></li>
+                                                <li><a href="javascript:void(0)"><i class="lni lni-trash"></i></a></li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                </div>
+                                <!-- End Single List -->
+                                <!-- Start Single List -->
+                                <div class="single-item-list">
+                                    <div class="row align-items-center">
+                                        <div class="col-lg-5 col-md-5 col-12">
+                                            <div class="item-image">
+                                                <img src="https://via.placeholder.com/100x100" alt="#">
+                                                <div class="content">
+                                                    <h3 class="title"><a href="javascript:void(0)">Brand New Iphone
+                                                            11
+                                                            Pro Max</a></h3>
+                                                    <span class="price">$800</span>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="col-lg-3 col-md-3 col-12">
+                                            <p>Electronic</p>
+                                        </div>
+                                        <div class="col-lg-2 col-md-2 col-12">
+                                            <p>New</p>
+                                        </div>
+                                        <div class="col-lg-2 col-md-2 col-12 align-right">
+                                            <ul class="action-btn">
+                                                <li><a href="javascript:void(0)"><i class="lni lni-eye"></i></a></li>
+                                                <li><a href="javascript:void(0)"><i class="lni lni-trash"></i></a>
+                                                </li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                </div>
+                                <!-- End Single List -->
+                                <!-- Pagination -->
+                                <div class="pagination left">
+                                    <ul class="pagination-list">
+                                        <li><a href="javascript:void(0)">1</a></li>
+                                        <li class="active"><a href="javascript:void(0)">2</a></li>
+                                        <li><a href="javascript:void(0)">3</a></li>
+                                        <li><a href="javascript:void(0)">4</a></li>
+                                        <li><a href="javascript:void(0)"><i class="lni lni-chevron-right"></i></a>
+                                        </li>
+                                    </ul>
+                                </div>
+                                <!--/ End Pagination -->
+                            </div>
+                            <!-- End Items Area -->
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- End Dashboard Section -->
+
+
+
+
+
     <section class="category-page section mt-4">
         <div class="container">
             <div class="row">
@@ -11,38 +189,44 @@
                                         <div class="col-lg-6 col-md-6 col-12">
                                             <h5>I miei articoli preferiti</h5>
                                         </div>
-                                        </div>
+                                    </div>
                                 </div>
                                 <div class="tab-content" id="nav-tabContent">
                                     <div class="tab-pane fade show active" id="nav-grid" role="tabpanel"
                                         aria-labelledby="nav-grid-tab">
                                         <div class="row">
-                                            @if($whishlists->isEmpty())
+                                            @if ($whishlists->isEmpty())
                                                 <p>Non hai ancora aggiunto articoli ai tuoi preferiti</p>
                                             @else
-                                            @foreach($whishlists as $whishlist)
-                                            <div class="col-lg-4 col-md-6 col-12">
-                                                <!-- Start Single Item -->
-                                                <div class="single-item-grid">
-                                                    <div class="image">
-                                                        <a href="item-details.html"><img src="{{ $whishlist->article->image ?? 'https://via.placeholder.com/150' }}" alt="{{ $whishlist->article->title }}"></a>
+                                                @foreach ($whishlists as $whishlist)
+                                                    <div class="col-lg-4 col-md-6 col-12">
+                                                        <!-- Start Single Item -->
+                                                        <div class="single-item-grid">
+                                                            <div class="image">
+                                                                <a href="item-details.html"><img
+                                                                        src="{{ $whishlist->article->image ?? 'https://via.placeholder.com/150' }}"
+                                                                        alt="{{ $whishlist->article->title }}"></a>
+                                                            </div>
+                                                            <div class="content">
+                                                                <a href="javascript:void(0)"
+                                                                    class="tag">{{ $whishlist->article->category->name }}</a>
+                                                                <h3 class="title">
+                                                                    <a
+                                                                        href="item-details.html">{{ $whishlist->article->title }}</a>
+                                                                </h3>
+                                                                <p class="location"><a href="javascript:void(0)"><i
+                                                                            class="lni lni-map-marker">
+                                                                        </i>{{ $whishlist->article->country }}</a></p>
+                                                                <ul class="info">
+                                                                    <li class="price">
+                                                                        {{ $whishlist->article->price }}€</li>
+                                                                    {{-- <li class="ms-auto mt-2"><p class="update-time">{{ $whishlist->$article->created_at->locale('it_IT')->isoFormat('DD MMM YYYY') }}</p></li> --}}
+                                                                </ul>
+                                                            </div>
+                                                        </div>
+                                                        <!-- End Single Item -->
                                                     </div>
-                                                    <div class="content">
-                                                        <a href="javascript:void(0)" class="tag">{{ $whishlist->article->category->name }}</a>
-                                                        <h3 class="title">
-                                                            <a href="item-details.html">{{ $whishlist->article->title }}</a>
-                                                        </h3>
-                                                        <p class="location"><a href="javascript:void(0)"><i class="lni lni-map-marker">
-                                                                </i>{{ $whishlist->article->country }}</a></p>
-                                                        <ul class="info">
-                                                            <li class="price">{{ $whishlist->article->price }}€</li>
-                                                            {{-- <li class="ms-auto mt-2"><p class="update-time">{{ $whishlist->$article->created_at->locale('it_IT')->isoFormat('DD MMM YYYY') }}</p></li> --}}
-                                                        </ul>
-                                                    </div>
-                                                </div>
-                                                <!-- End Single Item -->
-                                            </div>
-                                            @endforeach
+                                                @endforeach
                                             @endif
                                         </div>
                                         <div class="row">
@@ -54,7 +238,8 @@
                                                         <li class="active"><a href="javascript:void(0)">2</a></li>
                                                         <li><a href="javascript:void(0)">3</a></li>
                                                         <li><a href="javascript:void(0)">4</a></li>
-                                                        <li><a href="javascript:void(0)"><i class="lni lni-chevron-right"></i></a></li>
+                                                        <li><a href="javascript:void(0)"><i
+                                                                    class="lni lni-chevron-right"></i></a></li>
                                                     </ul>
                                                 </div>
                                                 <!--/ End Pagination -->
@@ -73,11 +258,11 @@
     {{-- <section class="wishlist section py-5 mt-5 pt-5 vh-100">
         <div class="container mt-5">
             <h2 class="mb-4">I miei preferiti</h2>
-            @if($whishlists->isEmpty())
+            @if ($whishlists->isEmpty())
             <p>Non hai ancora aggiunto articoli ai tuoi preferiti</p>
             @else
             <div class="row">
-                @foreach($whishlists as $whishlist)
+                @foreach ($whishlists as $whishlist)
                 <div class="col-lg-4 col-md-6 col-sm-12 mb-4">
                     <div class="card">
                         <img src="{{ $whishlist->article->image ?? 'https://via.placeholder.com/150' }}" class="card-img-top" alt="{{ $whishlist->article->title }}">
