dec0922249d622dcfb3ddfe4eb031311a0a33b91
Pasquale Abate <lino.abate@gmail.com>
2024-07-17 02:28:01 +0200

    modifica sidebar

--- resources/views/components/user/sidebar.blade.php
+++ resources/views/components/user/sidebar.blade.php
@@ -8,13 +8,19 @@
             <li><a class="{{ request()->routeIs('users.dashboard') ? 'active' : '' }}" href="{{ route('users.dashboard') }}"><i class="lni lni-dashboard"></i> Dashboard</a></li>
             <li><a class="{{ request()->routeIs('users.profile_settings') ? 'active' : '' }}" href="{{ route('users.profile_settings') }}"><i class="lni lni-pencil-alt"></i> Modifica Profilo</a></li>
             <li><a class="{{ request()->routeIs('users.my_items') ? 'active' : '' }}" href="{{ route('users.my_items') }}"><i class="lni lni-checkmark-circle"></i> I miei annunci</a></li>
-            <li><a href="{{ route('wishlist.index') }}"><i class="lni lni-heart"></i> Wishlist</a></li>
+            <li><a class="{{ request()->routeIs('wishlist.index') ? 'active' : '' }}" href="{{ route('wishlist.index') }}"><i class="lni lni-heart"></i> Wishlist</a></li>
             <li><a class="{{ request()->routeIs('articles.create') ? 'active' : '' }}" href="{{ route('articles.create') }}"><i class="lni lni-circle-plus"></i> Crea un annuncio</a></li>
             @if (auth()->user()->is_revisor)
                 <li>
                     <a class="{{ request()->routeIs('revisor.index') ? 'active' : '' }}" href="{{ route('revisor.index') }}">
-                        <i class="lni lni-write"></i> 
-                        <span class="badge">{{ $articles_to_check_count ?? '' }}</span> Annunci da revisionare
+                        <i class="lni lni-write"></i>
+                        Annunci da revisionare 
+                        @if ($articles_to_check_count)
+                        <span class="loader">{{ $articles_to_check_count}}</span>
+                        @else
+                            
+                        @endif
+                         
                     </a>
                 </li>
             @endif
