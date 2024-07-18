3217dac08a4025262b34d315991b9c7a63fbc164
Nikalo <nicola.alo88@gmail.com>
2024-07-18 20:56:15 +0200

    carosello revisore singola img no frecce

--- resources/views/revisor/index.blade.php
+++ resources/views/revisor/index.blade.php
@@ -48,6 +48,7 @@
                                                 
                                             @endif
                                         </div>
+                                        @if ($article_to_check->images->count()>1)
                                         <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
                                             <span class="carousel-control-prev-icon" aria-hidden="true"></span>
                                             <span class="visually-hidden">Previous</span>
@@ -56,6 +57,7 @@
                                             <span class="carousel-control-next-icon" aria-hidden="true"></span>
                                             <span class="visually-hidden">Next</span>
                                         </button>
+                                        @endif
                                     </div>  
                                 </div>
                             </div>
