628392c6fefcb91094819ee2cc7dbaa730a1d0e7
Lucrezia Caricchia <lucrezia.caricchia@gmail.com>
2024-07-19 17:11:12 +0200

    Fix frontend

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,6 +5,18 @@
     <!-- start login section -->
 
     <section class="login section">
+        <ul class="background">
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+        </ul>
         <div class="container">
             <div class="row mt-5">
                 <div class="col-lg-6 offset-lg-3 col-md-8 offset-md-2 col-12">
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -3,6 +3,19 @@
 
     <!-- start Registration section -->
     <section class="login registration section">
+
+        <ul class="background">
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+        </ul>
         <div class="container">
             <div class="row mt-5">
                 <div class="col-lg-6 offset-lg-3 col-md-8 offset-md-2 col-12">
