3dcaecbdfa5002bd0bdb712edce73c0505bbb670
Pasquale Abate <lino.abate@gmail.com>
2024-07-19 14:47:54 +0200

    fix vari

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -4,12 +4,12 @@
         <div class="el-wrapper single-grid wow fadeInUp" data-wow-delay=".2s">
             <div class="box-up">
                 <a href="{{ route('articles.show', compact('article')) }}" class="thumbnail"> <img class="img"
-                        src="{{ $article->images->isNotEmpty() ? Storage::url($article->images->first()->path) : 'https://picsum.photos/200' }}"
+                        src="{{ $article->images->isNotEmpty() ? $article->images->first()->getUrl(300,300) : 'https://picsum.photos/200' }}"
                         alt=""></a>
                 <div class="img-info">
                     <div class="info-inner">
                         <span class="p-name">{{ $article->title }}</span>
-                        <span class="p-company">{{ __('ui.Pubblicato da:') }} {{ $article->user->name }}</span>
+                        <span class="p-company">{{ __('ui.Pubblicato da:') }} {{ $article->user->firstName }}{{ $article->user->lastName }}</span>
                     </div>
                     <div class="a-size">{{ $article->created_at->locale('it_IT')->isoFormat('DD MMM YYYY') }}
                         <div class="info-list"><i class="lni lni-map-marker"></i>
@@ -36,40 +36,7 @@
             </div>
         </div>
     </div>
-    <!-- Start Single Grid -->
-    {{-- <div class="single-grid wow fadeInUp" data-wow-delay=".2s">
-        <div class="image">
-            <a href="{{ route('articles.show', compact('article')) }}" class="thumbnail"><img
-                    src="{{ asset('/assets/images/placeholder/scarpe-600x400.jpg') }}" alt="#"></a>
-            <div class="author"> --}}
-    {{-- <div class="author-image">
-                    <a href="javascript:void(0)"><img src="https://via.placeholder.com/100x100" alt="#">
-                        <span>Smith jeko</span></a>
-                </div>  --}}
-    {{-- <p class="sale">In vendita</p>
-            </div>
-        </div>
-        <div class="content">
-            <div class="top-content"> --}}
-    {{-- <a href="javascript:void(0)" class="tag">Telefoni</a> --}}
-    {{-- <p>{{ $article->category->name }}</p>
-                <h3 class="title">{{ $article->title }}</h3>
-                <p class="update-time">{{ $article->created_at->locale('it_IT')->isoFormat('DD MMM YYYY') }}</p>
-                <p class="user-name">Pubblicato da: {{ $article->user->name }}</p>
-                <ul class="info-list">
-                    <li><a href=""><i class="lni lni-map-marker"></i> {{ $article->city }}</a></li> --}}
-    {{-- <li><a href="javascript:void(0)"><i class="lni lni-timer"></i> Feb 18,
-                            2023</a></li> --}}
-    {{-- </ul>
-            </div>
-            <div class="bottom-content">
-                <p class="price">Prezzo: <span>{{ $article->price }}€</span></p>
-            </div>
-        </div>
-    </div> --}}
 
-
-    <!-- End Single Grid -->
 </div>
 
 
