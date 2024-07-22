3a85d3738d17921cded46f30822677aa66b15088
Lucrezia Caricchia <lucrezia.caricchia@gmail.com>
2024-07-22 22:03:41 +0200

    Global search fix

--- resources/views/livewire/global-search.blade.php
+++ resources/views/livewire/global-search.blade.php
@@ -1,20 +1,22 @@
-<div class="p-0 m-0 b-0">
-    <div class="d-flex justify-content-center align-items-center">
-        <div class="search-form wow fadeInUp cstsearch" data-wow-delay=".7s">
-            <div class="row">
-                <div class="col-12 p-0">
-                    <div class="search-input">
-                        <label for="keyword"><i class="lni lni-search-alt theme-color"></i></label>
-                        <input type="search" wire:model.live.debounce.300ms="query" id="globalInput"
-                            placeholder="{{ __('ui.Cerca prodotto') }}">
+<div class="container">
+
+    <div class="p-0 m-0 b-0">
+        <div class="d-flex justify-content-center align-items-center">
+            <div class="search-form wow fadeInUp cstsearch" data-wow-delay=".7s">
+                <div class="row">
+                    <div class="col-12 p-0">
+                        <div class="search-input">
+                            <label for="keyword"><i class="lni lni-search-alt theme-color"></i></label>
+                            <input type="search" wire:model.live.debounce.300ms="query" id="globalInput"
+                                placeholder="{{ __('ui.Cerca prodotto') }}">
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
-    <div class="container">
-        <div class="row justify-content-center">
-            @if ($articles)
+        @if ($articles)
+            <div class="row justify-content-center pt-4">
+
                 @foreach ($articles as $article)
                     <!-- Start Single Card -->
                     <div class="col-lg-2 col-md-3 col-sm-4 col-6 p-0 mx-1">
@@ -23,28 +25,34 @@
                                 <div class="card-body p-4">
                                     <img src="https://bootstrapious.com/i/snippets/sn-cards/shoes-1_gthops.jpg"
                                         alt="" class="img-fluid d-block mx-auto mb-3">
-                                    <h7 class="text-dark">{{ $article->title }}
-                        </a></h7>
+                                    <h7 class="text-dark">{{ $article->title }}</h7>
+                                </div>
+                            </div>
+                        </a>
                         <p class="small text-muted font-italic">{{ $article->price }} €</p>
                     </div>
-        </div>
-        </a>
-    </div>
-    @endforeach
-    <a href="{{ route('articles.index') }}">
-        <div class="col-lg-2 col-md-3 col-sm-4 col-6 p-0 mx-1 mt-3 d-flex align-items-center">
-            <div class="card rounded shadow-sm border-0 cardcst2">
-                <div class="card-body p-4 d-flex align-items-center">
+                @endforeach
 
-                    <i class="text-white lni lni-eye"></i>
-                    <h7 class="text-white">{{ __('ui.Mostra altri') }}
-    </a></h7>
-</div>
-</div>
-</div>
-</a>
-@endif
-<!-- End Single Card -->
-</div>
-</div>
+
+
+            </div>
+            <div class="row justify-content-center">
+                <div class="col-lg-2 col-md-3 col-sm-4 col-6 p-0 mx-1 mt-3 align-items-center">
+                    <div class="card shadow-sm border-0">
+                        <div class=" p-4 align-items-center rounded-1 button-show-more">
+                            <a href="{{ route('articles.index') }}">
+                                <i class="text-white lni lni-eye pe-2"></i>
+                                <h7 class="text-white">{{ __('ui.Mostra altri') }}</h7>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        @endif
+
+
+
+    </div>
+
 </div>
