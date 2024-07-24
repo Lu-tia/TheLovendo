8b8838fdfe8e1fabfff59b0964f30c3ee629bdb6
Pasquale Abate <lino.abate@gmail.com>
2024-07-24 20:59:50 +0200

    modifica review

--- resources/views/revisor/index.blade.php
+++ resources/views/revisor/index.blade.php
@@ -28,50 +28,96 @@
 
 
                         @if ($article_to_check)
-
-                        <div class="col-6 my-5">
-                            <div
-                                class="article-image {{ $article_to_check->images->count() == 0 ? 'h-100 d-flex align-items-center justify-content-center' : '' }} ">
-                                <div id="carouselExampleIndicators" class="carousel slide">
-                                    <div class="carousel-inner ">
-                                        @if ($article_to_check->images->count())
-                                        @foreach ($article_to_check->images as $key => $image)
-                                        <div class="carousel-item {{ $key == 0 ? 'active' : '' }}">
-                                            <img src="{{ $image->getUrl(300, 300) }}" class="d-block w-100" alt="...">
-                                            <div class="d-flex align-items-center justify-content-center flex-column">
-                                                <h5>Labels</h5>
-                                                @if ($image->labels)
-                                                @foreach ($image->labels as $label )
-                                                #{{$label}}
-                                                @endforeach
-                                                @else
-                                                <p>No labels</p>
-                                                @endif
-                                            </div>
+                        <div class="row">
+                            <div class="my-5 ps-4">
+                                <div class="article-image {{ $article_to_check->images->count() == 0 ? '  d-flex align-items-center justify-content-center' : '' }} ">
+                                    <div id="carouselExampleIndicators" class="carousel slide">
+                                        <div class="carousel-inner ">
+                                            @if ($article_to_check->images->count())
+                                            @foreach ($article_to_check->images as $key => $image)
+                                                <div class="carousel-item  {{ $key == 0 ? 'active' : '' }}">
+                                                    <div class="col-12 d-flex p-5">
+                                                        <div class="col-6">
+                                                            <img src="{{ $image->getUrl(300, 300) }}" class="d-block w-100" alt="...">
+                                                        </div>
+                                                        <div class="col-6">
+                                                            <div>
+                                                                <div class="text-end pe-1">
+                                                                    <h5>Valutazioni</h5>
+                                                                    <div class="row justify-content-center mt-2">
+                                                                        <div class="col-2">
+                                                                            <div class="text-center mx-auto {{$image->adult}}">
+                                                                            </div>
+                                                                        </div>
+                                                                        <div class="col-10">Contenuto per adulti</div>
+                                                                    </div>
+                                                                    <div class="row justify-content-center">
+                                                                        <div class="col-2">
+                                                                            <div class="text-center mx-auto {{$image->violence}}">
+                                                                            </div>
+                                                                        </div>
+                                                                        <div class="col-10">Violenza</div>
+                                                                    </div>
+                                                                    <div class="row justify-content-center">
+                                                                        <div class="col-2">
+                                                                            <div class="text-center mx-auto {{$image->spoof}}">
+                                                                            </div>
+                                                                        </div>
+                                                                        <div class="col-10">Contraffazione</div>
+                                                                    </div>
+                                                                    <div class="row justify-content-center">
+                                                                        <div class="col-2">
+                                                                            <div class="text-center mx-auto {{$image->racy}}">
+                                                                            </div>
+                                                                        </div>
+                                                                        <div class="col-10">Razzismo</div>
+                                                                    </div>
+                                                                    <div class="row justify-content-center">
+                                                                        <div class="col-2">
+                                                                            <div class="text-center mx-auto {{$image->medical}}">
+                                                                            </div>
+                                                                        </div>
+                                                                        <div class="col-10">Contenuti sensibili</div>
+                                                                    </div>
+                                                                </div>
+                                                            </div>
+                                                            <hr>
+                                                            <div class="row gap-2 ms-5">
+                                                                @if ($image->labels)
+                                                                @foreach ($image->labels as $label )
+                                                                <div class="col-3 tags d-flex shadow-sm">#{{$label}}</div>
+                                                                @endforeach
+                                                                @else
+                                                                <p>No labels</p>
+                                                                @endif
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
                                             
+                                            @endforeach
+                                            @else
+                                            <p class="h-100">Nessuna immagine caricata dall'utente</p>
+                                            @endif
                                         </div>
-                                        @endforeach
-                                        @else
-                                        <p class="h-100">Nessuna immagine caricata dall'utente</p>
+                                        @if ($article_to_check->images->count() > 1)
+                                        <button class="carousel-control-prev" type="button"
+                                            data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+                                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                            <span class="visually-hidden">Previous</span>
+                                        </button>
+                                        <button class="carousel-control-next" type="button"
+                                            data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+                                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                            <span class="visually-hidden">Next</span>
+                                        </button>
                                         @endif
                                     </div>
-                                    @if ($article_to_check->images->count() > 1)
-                                    <button class="carousel-control-prev" type="button"
-                                        data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
-                                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                                        <span class="visually-hidden">Previous</span>
-                                    </button>
-                                    <button class="carousel-control-next" type="button"
-                                        data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
-                                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                                        <span class="visually-hidden">Next</span>
-                                    </button>
-                                    @endif
                                 </div>
                             </div>
                         </div>
 
-                        <div class="col-6 my-5">
+                        <div class="col-12 mb-5 d-flex justify-content-center">
                             <div class="article-details .descriptionCard">
                                 <h3>{{ $article_to_check->title }}</h3>
                                 <p>{{ $article_to_check->body }}</p>
@@ -79,63 +125,23 @@
                                 <p class="author">Autore: {{ $article_to_check->user->firstName }}
                                     {{ $article_to_check->user->lastName }}</p>
                             </div>
-                            <div class="article-actions d-flex justify-content-evenly">
-                                <form action="{{ route('accept', ['article' => $article_to_check]) }}" method="post" class="button">
+                            <div class="article-actions d-flex flex-column justify-content-center ms-2">
+                                <form action="{{ route('accept', ['article' => $article_to_check]) }}" method="post" class="button mb-4">
                                     @csrf
                                     @method('PATCH')
                                     <button class="btn me-3" type="submit">
                                         <span>{{ __('ui.ACCETTA') }}</span>
                                     </button>
                                 </form>
-                                <form action="{{ route('reject', ['article' => $article_to_check]) }}" method="post"  class="button">
+                                <form action="{{ route('reject', ['article' => $article_to_check]) }}" method="post"  class="button mb-2">
                                     @csrf
                                     @method('PATCH')
                                     <button class="btn btn-danger" type="submit">
                                         <span>{{ __('ui.RIFIUTA') }}</span>
                                     </button>
                                 </form>
-                            </div>
-                            <hr>
-                            <div>
-                                <div class="text-end pe-1">
-                                    <h5>Valutazioni</h5>
-                                    <div class="row justify-content-center mt-2">
-                                        <div class="col-2">
-                                            <div class="text-center mx-auto {{$image->adult}}">
-                                            </div>
-                                        </div>
-                                        <div class="col-10">Contenuto per adulti</div>
-                                    </div>
-                                    <div class="row justify-content-center">
-                                        <div class="col-2">
-                                            <div class="text-center mx-auto {{$image->violence}}">
-                                            </div>
-                                        </div>
-                                        <div class="col-10">Violenza</div>
-                                    </div>
-                                    <div class="row justify-content-center">
-                                        <div class="col-2">
-                                            <div class="text-center mx-auto {{$image->spoof}}">
-                                            </div>
-                                        </div>
-                                        <div class="col-10">Contraffazione</div>
-                                    </div>
-                                    <div class="row justify-content-center">
-                                        <div class="col-2">
-                                            <div class="text-center mx-auto {{$image->racy}}">
-                                            </div>
-                                        </div>
-                                        <div class="col-10">Razzismo</div>
-                                    </div>
-                                    <div class="row justify-content-center">
-                                        <div class="col-2">
-                                            <div class="text-center mx-auto {{$image->medical}}">
-                                            </div>
-                                        </div>
-                                        <div class="col-10">Contenuti sensibili</div>
-                                    </div>
-                                </div>
-                            </div>
+                            </div>       
+                        </div>
                         </div>
                         @else
                         <div class="col">
