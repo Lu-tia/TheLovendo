4b651b50f75c2382dd893e28effe8b43c907dccd
Gabriele Zucchi <19gabrielezucchi99@gmail.com>
2024-07-25 20:38:21 +0200

    fix lang

--- lang/it/ui.php
+++ lang/it/ui.php
@@ -135,7 +135,6 @@ return [
     'Inserisci la tua città' => 'Inserisci la tua città',
     'Inserisci la tua età' => 'Inserisci la tua età',
     'Visualizza articolo' => 'Visualizza articolo',
-<<<<<<< HEAD
     'Nessuna immagine caricata dall\'utente' => 'Nessuna immagine caricata dall\'utente',
     'Prezzo:' => 'Prezzo:',
     'Autore:' => 'Autore:',
@@ -146,8 +145,6 @@ return [
     'Razzismo' => 'Razzismo',
     'Contenuti sensibili' => 'Contenuti sensibili',
 
-=======
     "Esistono molte varianti dei passaggi di TheLovendo disponibile, ma la maggior parte ha subito alterazioni in qualche forma." => "Esistono molte varianti dei passaggi di TheLovendo disponibile, ma la maggior parte ha subito alterazioni in qualche forma.",
->>>>>>> 0f6f025c6d8b67e8bb8b37231f9d9e818472400c
 
 ];
